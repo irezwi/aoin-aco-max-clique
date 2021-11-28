a7e62c0693257f6c9277fe78aa327e5f18fadfdf
irezwi <zwirek.ireneusz@gmail.com>
2021-11-28 20:42:16 +0100

    Add clique implementation; Extend tests coverage

--- graph.py
+++ graph.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from functools import total_ordering
-from typing import Set
+from typing import Set, Iterable
 
 from scipy.io import mmread
 
@@ -9,9 +9,10 @@ from scipy.io import mmread
 @dataclass(eq=True, frozen=True, unsafe_hash=True)
 class Node:
     idx: int
+    pheromone: float = 0.0
 
     def __lt__(self, other) -> bool:
-        return self.idx < other.idx
+        return self.pheromone < other.pheromone
 
 
 @dataclass(frozen=True)
@@ -21,13 +22,16 @@ class Edge:
 
     @property
     def nodes(self) -> Set[Node]:
+        """
+        :return: Set of two nodes that create the edge
+        """
         return {self.node_a, self.node_b}
 
     def __eq__(self, other) -> bool:
         return {self.node_a, self.node_b} == {other.node_a, other.node_b}
 
     def __hash__(self):
-        return hash(tuple(sorted(self.nodes)))
+        return hash(tuple(sorted(self.nodes, key=lambda n: n.idx)))
 
 
 class Graph:
@@ -36,15 +40,44 @@ class Graph:
         self.nodes: Set[Node] = set()
 
     def add_edge(self, edge: Edge) -> None:
+        """
+        Adds edge to the graph
+
+        :param edge: Edge to be added
+        """
         if edge not in self.edges:
             self.edges.add(edge)
-            self.nodes.update(edge.nodes)
+            self.add_nodes(edge.nodes)
+
+    def add_node(self, node: Node) -> None:
+        """
+        Adds node to the graph
+
+        :param node: Node to be added
+        """
+        self.nodes.add(node)
+
+    def add_nodes(self, nodes: Iterable[Node]) -> None:
+        """
+        Adds nodes to the graph
+
+        :param nodes: Collection of nodes to be added
+        """
+        self.nodes.update(nodes)
 
     def __repr__(self) -> str:
         return f'Graph({len(self.nodes)}, {len(self.edges)})'
 
     @classmethod
     def read_from_file(cls, filepath):
+        """
+        Instantiates new Graph object initializes it with data provided in `filepath` file.
+
+        :param filepath: File containing graph data
+        :type filepath: str
+        :return: New Graph instance
+        :rtype: Graph
+        """
         g = mmread(filepath)
         instance = cls()
 
@@ -54,3 +87,55 @@ class Graph:
             instance.add_edge(edge)
 
         return instance
+
+    def get_node_edges(self, node: Node) -> Set[Edge]:
+        """
+        :param node: Node which edges set should be returned
+        :returns: Set of edges connected to `node`
+        """
+        return {edge for edge in self.edges if node in edge.nodes}
+
+
+class CliqueConstraintViolationError(Exception):
+    """ Raised when any clique constraint is violated """
+    pass
+
+
+class Clique:
+    def __init__(self, graph):
+        self.edges: Set[Edge] = set()
+        self.nodes: Set[Node] = set()
+        self.graph: Graph = graph
+
+    def __is_connected_with_all_nodes(self, node: Node) -> bool:
+        """
+        Checks if all nodes that belongs to clique are connected by any edge to `node`
+
+        :param node: Node for which check should be executed
+        :return: True if all clique nodes are connected to `node`, False otherwise
+        """
+        return all(
+            Edge(existing_node, node) in self.graph.edges for existing_node in self.nodes
+        )
+
+    def add_node(self, node: Node):
+        """
+        Adds `node` to clique. Also adds edges between `node` and existing clique nodes.
+
+        :param node: Node to be added to clique
+        :raises CliqueConstraintViolationError: if node given as argument cannot be added to clique
+        """
+        if self.__is_connected_with_all_nodes(node):
+            self.nodes.add(node)
+            self.edges.update(
+                {Edge(existing_node, node) for existing_node in filter(lambda n: n != node, self.nodes)}
+            )
+        else:
+            raise CliqueConstraintViolationError(
+                f"Cannot add node {node} because it's not connected to all existing nodes")
+
+    def get_candidates(self):
+        graph_nodes = sorted(self.graph.nodes, key=lambda node: node.idx)
+        possible_candidates = [node for node in graph_nodes if self.__is_connected_with_all_nodes(node)]
+
+        return sorted(possible_candidates)
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,11 +1,47 @@
+from itertools import combinations
+from os.path import join
 import pytest
-from graph import Graph, Node, Edge
+from graph import Graph, Node, Edge, Clique, CliqueConstraintViolationError
 
 
 @pytest.fixture(scope='function')
 def default_graph():
+    """ Empty graph """
     graph = Graph()
     return graph
+
+
+@pytest.fixture(scope='function')
+def k5_graph():
+    """
+    Graph:
+        1 - 2
+        | X |
+        3 - 4
+    """
+    graph = Graph()
+    nodes = [Node(i) for i in range(5)]
+    graph.add_nodes(nodes)
+    for start_node, end_node in combinations(range(5), 2):
+        edge = Edge(Node(start_node), Node(end_node))
+        graph.add_edge(edge)
+    yield graph
+
+
+@pytest.fixture(scope='function')
+def k5_plus_one(k5_graph):
+    """
+    Graph:
+        1 - 2
+        | X |
+        3 - 4 - 5
+    """
+    graph = k5_graph
+    graph.add_node(Node(5))
+    graph.add_edge(
+        Edge(Node(4), Node(5))
+    )
+    yield graph
 
 
 def test_indirect_edges(default_graph):
@@ -23,3 +59,56 @@ def test_indirect_edges(default_graph):
     assert n2 in graph.nodes
     assert from_n1_to_n2 in graph.edges
     assert from_n2_to_n1 in graph.edges
+
+
+def test_get_node_edges(k5_graph):
+    graph = k5_graph
+
+    expected_edges = {Edge(Node(0), Node(i)) for i in range(1, 5)}
+    assert graph.get_node_edges(Node(0)) == expected_edges
+
+
+@pytest.mark.parametrize(
+    "file, expected_nodes, expected_edges",
+    [
+        ('soc-dolphins.mtx', 62, 159),
+        ('keller5.mtx', 776, 225990),
+    ]
+)
+def test_read_from_file(file, expected_nodes, expected_edges):
+    filepath = join('..', 'input', file)
+    graph = Graph.read_from_file(filepath)
+
+    assert len(graph.nodes) == expected_nodes
+    assert len(graph.edges) == expected_edges
+
+
+def test_clique(k5_graph):
+    clique = Clique(graph=k5_graph)
+
+    for node in clique.graph.nodes:
+        clique.add_node(node)
+
+    assert len(clique.nodes) == 5
+    assert len(clique.edges) == 10
+
+
+def test_error_on_invalid_add(k5_plus_one):
+    graph = k5_plus_one
+    clique = Clique(graph=graph)
+    correct_nodes = [Node(i) for i in range(5)]
+    invalid_node = Node(5)
+
+    for node in correct_nodes:
+        clique.add_node(node)
+
+    with pytest.raises(CliqueConstraintViolationError):
+        clique.add_node(invalid_node)
+
+
+def test_get_candidates(k5_graph):
+    clique = Clique(graph=k5_graph)
+
+    for node in clique.graph.nodes:
+        clique.add_node(node)
+        assert set(clique.get_candidates()) == clique.graph.nodes.difference(clique.nodes)
