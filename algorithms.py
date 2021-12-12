5120726e9418fdec560b6d0767b81683e956bdd4
Wojciech Czarnecki <48015260+s235714@users.noreply.github.com>
2021-12-12 23:37:05 +0100

    Trochę błędów

--- algorithms.py
+++ algorithms.py
@@ -1,9 +1,11 @@
 from abc import ABCMeta, abstractmethod
+import random
 
 
 class Algorithm(metaclass=ABCMeta):
-    def __init__(self, graph):
+    def __init__(self, graph, output):
         self.graph = graph
+        self.graph = output
 
     @abstractmethod
     def run(self):
@@ -21,6 +23,24 @@ class AntColonyOptimizerAlgorithm(Algorithm):
 
 
 class ReferenceAlgorithm(Algorithm):
+    def __init__(self, graph, iterations):
+        super().__init__(graph, output)
+        self.iterations = iterations
 
     def run(self):
-        pass
+
+        agents = []
+        should_stop = False
+
+        while not should_stop:
+            for ag in agents:
+                candidates = ag.cliqe.get_candidates()
+                if not candidates:
+                    ag.finished = True
+                else:
+                    next_node = random.choice(candidates)
+                    ag.clique.add_node(next_node)
+                    print(f'Maksymalna klika:')
+                    print(ag)
+            self.iterations -= 1
+            should_stop = all(map(lambda x: x.finished, agents)) or self.iterations == 0
